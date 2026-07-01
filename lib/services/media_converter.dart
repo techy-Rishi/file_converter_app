45112140211a4d230be51dcdda3c820aea1e7451
Rishi Yadav <rahulrishiyadav1994@gmail.com>
2026-07-01 14:04:28 +0530

    Add files via upload

--- lib/services/media_converter.dart
+++ lib/services/media_converter.dart
@@ -1,6 +1,6 @@
 import 'dart:io';
-import 'package:ffmpeg_kit_flutter/ffmpeg_kit.dart';
-import 'package:ffmpeg_kit_flutter/return_code.dart';
+import 'package:ffmpeg_kit_flutter_new/ffmpeg_kit.dart';
+import 'package:ffmpeg_kit_flutter_new/return_code.dart';
 import 'package:path_provider/path_provider.dart';
 
 /// Converts audio/video files using FFmpeg.
